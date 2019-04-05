e16c9296d9b67eec899832bb3fd96040b94ca082
Korkmatik <fahri.korkmaz@protonmail.ch>
2019-04-06 00:28:32 +0200

    Removed unnecessary code

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,13 +2,6 @@
 
 #include "Game.hpp"
 
-void print(const std::vector<Vector2>& pos)
-{
-	for(int i=0; i<pos.size(); i++) {
-		std::cout << "Pos #" << i << " (" << pos[i].x << "/" << pos[i].y << ")\n";
-	}
-}
-
 int main()
 {
 	Game snakeGame{};
